d380342ee0ddc2802745d0e3e377a155f08337de
Nicole Dow <nikki.dow71@gmail.com>
2019-07-22 20:44:50 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    array_of_sentences = self.split(/[.!?]+/)
+    array_of_sentences.length
   end
 end
